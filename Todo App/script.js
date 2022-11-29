6bbef6448ebfa547c009e10f596732cc3edeae5d
nilesh0901 <84271025+nilesh0901@users.noreply.github.com>
2022-11-30 04:16:39 +0530

    Added Del function

--- Todo App/script.js
+++ Todo App/script.js
@@ -17,19 +17,45 @@ function generateItems(items){
     items.forEach((item) => {
         let todoItem = document.createElement("div");
         todoItem.classList.add("todo-item");
+        todoItem.setAttribute('data-id',item.id);
+        let xx = todoItem.getAttribute('data-id');
+        // console.log(xx)
         let checkContainer = document.createElement("div");
         checkContainer.classList.add("check");
         let checkMark = document.createElement("div");
         checkMark.classList.add("check-mark");
         checkMark.innerHTML = '<img src="assets/icon-check.svg">';
+        let checkDelete = document.createElement("div");
+        checkDelete.classList.add("delete-data");
+        checkDelete.innerHTML = '<img width="20" height="20" src="assets/delete.png">';
         checkMark.addEventListener("click", function(){
-            markCompleted(item.id);
+          markCompleted(item.id);  
         })
         checkContainer.appendChild(checkMark);
-
+        
+        checkDelete.addEventListener("click", (e) => {
+            // console.log(e.target)
+            e.stopPropagation()
+            let id1 = e.target.parentElement.parentElement.getAttribute('data-id');
+            // console.log(id1);
+            db.collection('todo-items').doc(id1).delete();
+            console.log("Data Deleted Successfully")
+            
+        })
+        
         let todoText = document.createElement("div");
         todoText.classList.add("todo-text");
         todoText.innerText = item.text;
+
+        x = document.querySelector(".items-left");
+        len = items.length
+        if (!len){
+            x.innerHTML = "0 items left"
+        }
+        else {
+            x.innerHTML = len.toString() +" items left"
+        }
+        
 
         if(item.status == "completed"){
             checkMark.classList.add("checked");
@@ -37,20 +63,27 @@ function generateItems(items){
         }
         todoItem.appendChild(checkContainer);
         todoItem.appendChild(todoText);
+        todoItem.appendChild(checkDelete);
         todoItems.push(todoItem)
     })
     document.querySelector(".todo-items").replaceChildren(...todoItems);
 }
-
-
+ 
 
 function addItem(event){
     event.preventDefault();
     let text = document.getElementById("todo-input");
-    let newItem = db.collection("todo-items").add({
+    if (text.value != ""){
+        let newItem = db.collection("todo-items").add({
         text: text.value,
         status: "active"
+        
     })
+    }
+    else{
+        alert("Please Add a task");
+    }
+    
     text.value = "";
 }
 
