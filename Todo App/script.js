006084a1bee96d3ffe649dc8a04813692db2243f
nilesh0901 <nilesh.k0901@gmail.com>
2022-11-29 19:27:33 +0530

    firebase added

--- Todo App/script.js
+++ Todo App/script.js
@@ -1,19 +1,83 @@
-// const firebaseConfig = { 
-//     apiKey: "AIzaSyByDKUYzGF0N5yt5XcQ-EU1-eS8b9Xprw4", 
-//     authDomain: "oib-todo-app.firebaseapp.com", 
-//     databaseURL: "https://oib-todo-app-default-rtdb.firebaseio.com",
-//     projectId: "oib-todo-app", 
-//     storageBucket: "oib-todo-app.appspot.com", 
-//     messagingSenderId: "642622619435", 
-//     appId: "1:642622619435:web:02af0fa690a2ad13754f6e", 
-//     measurementId: "G-3JMEQVKLYM" 
-// }; 
-// //initialize firebase
-// firebase.initializeApp(firebaseConfig)
-// //reference your database
+function addItem(event) {
+    event.preventDefault();
+    let text_inputed = document.getElementById("todo-input")
+    db.collection("todo-items").add({
+        text: text_inputed.value,
+        status: "active"
+    })
+    text_inputed = "";
+}
+
+function getItems() {
+    db.collection("todo-items").onSnapshot((snapshot) => {
+        console.log(snapshot)
+        let items = [];
+        snapshot.docs.forEach((doc) => {
+            items.push({
+                id: doc.id,
+                ...doc.data()
+            })
+        });
+        generateItems(items);
+    })
+}
+
+
+function generateItems(items) {
+
+    let itemsHTML = "";
+    items.forEach((item) => {
+        console.log(item)
+        itemsHTML += `
+            <div class="todo-item">
+                <div class="check">
+                    <div data-id="${item.id}" class="check-mark ${item.status == "completed" ? "checked": ""}" >
+                        <img style="height: 12px;" src="img/tick.png" alt="">
+                    </div>
+                </div>
+                <div class="todo-text">
+                    ${item.text}
+                </div>
+            </div>
+        `
+    })
+    document.querySelector(".todo-items").innerHTML = itemsHTML;
+    createEventListener();
+}
+
+function createEventListener() {
+    let todoCheckMarks = document.querySelectorAll(".todo-item .check-mark");
+    todoCheckMarks.forEach((checkMark) => {
+        checkMark.addEventListener("click", function() {
+            markCompleted(checkMark.dataset.id);
+        })
+    })
+}
 
 function addItem(event) {
     event.preventDefault();
-    let text = document.getElementById("todo-input")
-    console.log(text.value)
+    let text = document.getElementById("todo-input");
+    let newItem = db.collection("todo-items").add({
+        text: text.value,
+        status: "active"
+    })
+    text.value = "";
 }
+
+function markCompleted(id) {
+    let item = db.collection("todo-items").doc(id);
+    item.get().then(function(doc) {
+        if (doc.exists) {
+            if (doc.data().status == "active") {
+                item.update({
+                    status: "completed"
+                })
+            } else {
+                item.update({
+                    status: "active"
+                })
+            }
+        }
+    })
+}
+getItems();
