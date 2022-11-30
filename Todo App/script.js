fb81d6eba096b9abf3b8b076e23046d96806c190
nilesh0901 <nilesh.k0901@gmail.com>
2022-11-30 23:17:31 +0530

    task3

--- Todo App/script.js
+++ Todo App/script.js
@@ -1,23 +1,57 @@
+let active_items = [];
+let completed_items = [];
 
-function getItems(){
+
+function getItems() {
     db.collection("todo-items").onSnapshot((snapshot) => {
         let items = [];
         snapshot.docs.forEach((doc) => {
             items.push({
-                id: doc.id, 
+                id: doc.id,
                 ...doc.data()
             })
         })
+        console.log(items)
+
+        items.forEach((x) => {
+            if (x.status == "active") {
+                active_items.push(x)
+            } else {
+                completed_items.push(x)
+            }
+        })
+
+        console.log(active_items)
+        console.log(completed_items)
+
         generateItems(items);
+
+        let all = document.querySelector(".all")
+        all.addEventListener("click", (e) => {
+            generateItems(items);
+        })
+
+        let active_only = document.querySelector(".only-active")
+        active_only.addEventListener("click", (e) => {
+            generateItems(active_items);
+        })
+
+        let completed_only = document.querySelector(".only-completed")
+        completed_only.addEventListener("click", (e) => {
+            generateItems(completed_items);
+        })
+
+
+
     })
 }
 
-function generateItems(items){
+function generateItems(items) {
     let todoItems = []
     items.forEach((item) => {
         let todoItem = document.createElement("div");
         todoItem.classList.add("todo-item");
-        todoItem.setAttribute('data-id',item.id);
+        todoItem.setAttribute('data-id', item.id);
         let xx = todoItem.getAttribute('data-id');
         // console.log(xx)
         let checkContainer = document.createElement("div");
@@ -28,11 +62,11 @@ function generateItems(items){
         let checkDelete = document.createElement("div");
         checkDelete.classList.add("delete-data");
         checkDelete.innerHTML = '<img width="20" height="20" src="assets/delete.png">';
-        checkMark.addEventListener("click", function(){
-          markCompleted(item.id);  
+        checkMark.addEventListener("click", function() {
+            markCompleted(item.id);
         })
         checkContainer.appendChild(checkMark);
-        
+
         checkDelete.addEventListener("click", (e) => {
             // console.log(e.target)
             e.stopPropagation()
@@ -40,27 +74,40 @@ function generateItems(items){
             // console.log(id1);
             db.collection('todo-items').doc(id1).delete();
             console.log("Data Deleted Successfully")
-            
+
         })
-        
+
         let todoText = document.createElement("div");
         todoText.classList.add("todo-text");
         todoText.innerText = item.text;
 
         x = document.querySelector(".items-left");
         len = items.length
-        if (!len){
+        if (!len) {
             x.innerHTML = "0 items left"
+        } else {
+            x.innerHTML = len.toString() + " items left"
         }
-        else {
-            x.innerHTML = len.toString() +" items left"
-        }
-        
 
-        if(item.status == "completed"){
+        let clear_completed = document.querySelector(".clear-all")
+        clear_completed.addEventListener("click", (e) => {
+            // console.log(e.target)
+            e.stopPropagation()
+            completed_items.forEach((ee) => {
+                    db.collection('todo-items').doc(ee.id).delete();
+                })
+                // console.log(id1);
+
+            console.log("All completed Data Deleted Successfully")
+
+        })
+
+
+        if (item.status == "completed") {
             checkMark.classList.add("checked");
             todoText.classList.add("checked");
         }
+
         todoItem.appendChild(checkContainer);
         todoItem.appendChild(todoText);
         todoItem.appendChild(checkDelete);
@@ -68,30 +115,29 @@ function generateItems(items){
     })
     document.querySelector(".todo-items").replaceChildren(...todoItems);
 }
- 
 
-function addItem(event){
+
+function addItem(event) {
     event.preventDefault();
     let text = document.getElementById("todo-input");
-    if (text.value != ""){
+    if (text.value != "") {
         let newItem = db.collection("todo-items").add({
-        text: text.value,
-        status: "active"
-        
-    })
-    }
-    else{
+            text: text.value,
+            status: "active"
+
+        })
+    } else {
         alert("Please Add a task");
     }
-    
+
     text.value = "";
 }
 
-function markCompleted(id){
+function markCompleted(id) {
     let item = db.collection("todo-items").doc(id);
     item.get().then(function(doc) {
         if (doc.exists) {
-            if(doc.data().status == "active"){
+            if (doc.data().status == "active") {
                 item.update({
                     status: "completed"
                 })
